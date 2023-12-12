7f27ac612426f320a8b83c82cdd152ba143236d4
superkkj <blood_nail@naver.com>
2023-12-13 01:24:43 +0900

    feat :  jpa sql 로그 출력 fomatter기능 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,9 @@ subprojects {
 		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
+		implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.0")
+
+
 		testImplementation("org.springframework.boot:spring-boot-starter-test")
 		testImplementation("org.springframework.security:spring-security-test")
 	}
