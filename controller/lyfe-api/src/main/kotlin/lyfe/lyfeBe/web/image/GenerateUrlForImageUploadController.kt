62166ac06dd5a4d94a1ad74336249a9693c6dd5a
sectionr0 <sectionr0@gmail.com>
2024-02-20 00:02:17 +0900

    fix: 업로드 오류 수정

--- controller/lyfe-api/src/main/kotlin/lyfe/lyfeBe/web/image/GenerateUrlForImageUploadController.kt
+++ controller/lyfe-api/src/main/kotlin/lyfe/lyfeBe/web/image/GenerateUrlForImageUploadController.kt
@@ -1,22 +1,24 @@
-//package lyfe.lyfeBe.web.image
-//
-//import lyfe.lyfeBe.dto.CommonResponse
-//import lyfe.lyfeBe.image.port.`in`.GenerateImageUploadUseCase
-//import lyfe.lyfeBe.image.GetImageUploadUrlResult
-//import org.springframework.web.bind.annotation.*
-//
-//@RestController
-//class GenerateUrlForImageUploadController(
-//    private val generateImageUploadUseCase: GenerateImageUploadUseCase
-//) {
-//
-//    @GetMapping("/v1/images/get-upload-url")
-//    fun getUploadUrl(
-//        @RequestParam format: String,
-//        @RequestParam path: String
-//    ): CommonResponse<GetImageUploadUrlResult> {
-//
-//        return CommonResponse(generateImageUploadUseCase.generateImageUploadUrl(format = format, path = path))
-//    }
-//
-//}
+package lyfe.lyfeBe.web.image
+
+import lyfe.lyfeBe.dto.CommonResponse
+import lyfe.lyfeBe.image.GetImageUploadUrlResult
+import lyfe.lyfeBe.image.port.`in`.GenerateImageUploadUseCase
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GenerateUrlForImageUploadController(
+    private val generateImageUploadUseCase: GenerateImageUploadUseCase
+) {
+
+    @GetMapping("/v1/images/get-upload-url")
+    fun getUploadUrl(
+        @RequestParam format: String,
+        @RequestParam path: String
+    ): CommonResponse<GetImageUploadUrlResult> {
+
+        return CommonResponse(generateImageUploadUseCase.generateImageUploadUrl(format = format, path = path))
+    }
+
+}
