d3439e2b0413ccd307cf0eb2dcfadbe84580e6f5
sectionr0 <sectionr0@gmail.com>
2024-06-19 01:03:12 +0900

    feat: 카카오 연결 끊기 url 변경

--- usecase/src/main/kotlin/lyfe/lyfeBe/auth/service/kakao/KakaoApiClient.kt
+++ usecase/src/main/kotlin/lyfe/lyfeBe/auth/service/kakao/KakaoApiClient.kt
@@ -1,11 +1,14 @@
 package lyfe.lyfeBe.auth.service.kakao
 
 import lyfe.lyfeBe.aop.FeignClientConfiguration
+import lyfe.lyfeBe.auth.dto.kakao.KakaoRevokeResult
 import lyfe.lyfeBe.auth.dto.kakao.KakaoUserInfoResult
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestParam
 
 @FeignClient(name = "kakaoApi", url = "https://kapi.kakao.com", configuration = [KakaoApiHeaderConfiguration::class, FeignClientConfiguration::class])
 interface KakaoApiClient {
@@ -18,4 +21,16 @@ interface KakaoApiClient {
     fun getUserInfo(
         @RequestHeader("Authorization") authorization: String
     ): KakaoUserInfoResult
+
+
+    /**
+     * 연결 끊기
+     * https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#req-unlink
+     */
+    @PostMapping("/v1/user/unlink", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun unlink(
+        @RequestHeader("Authorization") adminKey: String,
+        @RequestParam("target_id_type") targetIdType: String,
+        @RequestParam("target_id") targetId: String
+    ): KakaoRevokeResult
 }
